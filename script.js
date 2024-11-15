3ab3d5ca3297cca4fb4ccd10d2b8454b51f2111d
Albertbarkovski <albertas.barkovski@gmail.com>
2024-11-15 13:37:50 +0200

    Refactor navigation structure and enhance tab functionality; update styles for header and sections

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// console.log('Hello, world!');
+const buttons = document.querySelectorAll(".nav-link");
+const contents = document.querySelectorAll(".tab");
+const btnContainer = document.querySelector(".nav-container");
+
+// console.log(contents);
+// console.log(buttons);
+// console.log(btnContainer);
+
+btnContainer.addEventListener("click", (event) => {
+  const clickedButtonId = event.target.dataset.id;
+
+  // remove active class for all buttons (reset)
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+  });
+
+  // remove active class for all contents (reset)
+  contents.forEach((content) => {
+    content.classList.remove("active");
+  });
+
+  // add active class to the clicked button
+  event.target.classList.add("active");
+
+  // add active class to content
+  const content = document.getElementById(clickedButtonId);
+  content.classList.add("active");
+});
